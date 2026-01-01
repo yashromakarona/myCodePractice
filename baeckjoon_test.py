4b489661f8858fb7d51825ed6b1eed7900d1570c
yashromakarona <yashromakarona@users.noreply.github.com>
2026-01-01 21:37:22 +0900

    baekjoon:Oven Timer

---
+++ baeckjoon_test.py
@@ -0,0 +1,20 @@
+a = input()
+b = int(input())
+
+c = a.split()
+
+c[0] = int(c[0])
+c[1] = int(c[1])
+
+total_min = c[1] + b
+
+if total_min // 60 == 0:
+    c[1] = total_min
+    
+else:
+    c[0] += (total_min // 60)
+    c[1] = (total_min % 60)
+    
+print(c[0],c[1])
+ 
+    
